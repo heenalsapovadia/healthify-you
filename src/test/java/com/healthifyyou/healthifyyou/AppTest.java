59b4a6b028d3f8af42c07b226da1726d23b52945
Gurleen Kaur Saluja <gr997570@dal.ca>
2021-06-22 12:13:15 +0530

    Project Structure updated.

---
+++ src/test/java/com/healthifyyou/healthifyyou/AppTest.java
@@ -0,0 +1,20 @@
+package com.healthifyyou.healthifyyou;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
