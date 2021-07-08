c88007b8ca4cdc768f1a476f9beb1bf16c77b97e
Gurleen Kaur Saluja <gr997570@dal.ca>
2021-07-09 02:15:41 +0530

    CSCI5308-11 Functional Test Pack for Invoice Management (Feature Phase 1 Completed)

---
+++ src/test/java/presentation/admin/PharmaInvoiceOutputTest.java
@@ -0,0 +1,14 @@
+package presentation.admin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PharmaInvoiceOutputTest {
+
+	@Test
+	public void testDisplayInvoice() {
+		//fail("Not yet implemented");
+	}
+
+}
