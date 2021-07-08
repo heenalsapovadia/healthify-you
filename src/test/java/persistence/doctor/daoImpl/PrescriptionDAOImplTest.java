9ddb9a9abde426df1a2cf39a4832a763a636801c
heenalsapovadia <heenalsapovadia@gmail.com>
2021-07-08 14:44:44 +0530

    Ticket#CSCI5308-5 Setting up Test classes

---
+++ src/test/java/persistence/doctor/daoImpl/PrescriptionDAOImplTest.java
@@ -0,0 +1,18 @@
+package persistence.doctor.daoImpl;
+
+import junit.framework.TestCase;
+
+public class PrescriptionDAOImplTest extends TestCase {
+
+    /*
+    Check if prescriptions were inserted or not
+     */
+    public void testInsertPrescription() {
+    }
+
+    /*
+    fetch the max prescription id value from database table
+     */
+    public void testFindMaxPrescriptionId() {
+    }
+}
