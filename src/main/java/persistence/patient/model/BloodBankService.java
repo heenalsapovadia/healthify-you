192067fe08e9063539d95d4d3a622c1e41cab1d2
Saloni Madhusudan Raythatha <sl768578@dal.ca>
2021-07-09 10:34:57 +0530

    Updating model class for BloodBankServive.java

--- src/main/java/persistence/patient/model/BloodBankService.java
+++ src/main/java/persistence/patient/model/BloodBankService.java
@@ -1,4 +1,44 @@
 package persistence.patient.model;
 
+import java.util.Date;
+
 public class BloodBankService {
+
+    private int patientId;
+    private int DonationId;
+    private String BloodGrp;
+    private Date date;
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getDonationId() {
+        return DonationId;
+    }
+
+    public void setDonationId(int donationId) {
+        DonationId = donationId;
+    }
+
+    public String getBloodGrp() {
+        return BloodGrp;
+    }
+
+    public void setBloodGrp(String bloodGrp) {
+        BloodGrp = bloodGrp;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
--- src/main/java/persistence/patient/utilImpl/BloodBankUtilImpl.java
+++ src/main/java/persistence/patient/utilImpl/BloodBankUtilImpl.java
@@ -1,14 +1,18 @@
 package persistence.patient.utilImpl;
 
+import java.util.Date;
+
 public class BloodBankUtilImpl {
-// checking date validation
-//    Date current = new Date();
+ //checking date validation
+//    Date currentDate = new Date();
+//
 //    //create a date one day before current date
-//    long prevDay = System.currentTimeMillis() - 1000*60*60*24;
-//    //create date object
-//    Date prev = new Date(prevDay);
-//    //compare both dates
-//        if(prev.before(current)){
+//    long previousDay = System.currentTimeMillis() - 1000*60*60*24;
+//
+//
+//    Date prev = new Date(previousDay);
+
+//        if(previousDay.before(currentDate)){
 //        System.out.println("The date is older than current day");
 //    } else {
 //        System.out.println("The date is future day");
