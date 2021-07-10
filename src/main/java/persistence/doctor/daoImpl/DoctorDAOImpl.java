deb4463f29159e931bb38737d2aceddd56d9c638
heenalsapovadia <heenalsapovadia@gmail.com>
2021-07-10 11:26:58 +0530

    Ticket#CSCI5308-19 Added class structure and Tests for Doctor model

---
+++ src/main/java/persistence/doctor/daoImpl/DoctorDAOImpl.java
@@ -0,0 +1,11 @@
+package persistence.doctor.daoImpl;
+
+import persistence.doctor.dao.DoctorDAO;
+import persistence.doctor.model.Doctor;
+
+public class DoctorDAOImpl implements DoctorDAO {
+    @Override
+    public Doctor getDoctor(String email) {
+        return null;
+    }
+}
---
+++ src/main/java/persistence/doctor/model/Doctor.java
@@ -0,0 +1,97 @@
+package persistence.doctor.model;
+
+import java.util.Date;
+
+public class Doctor {
+    int doctor_id;
+    String first_name;
+    String last_name;
+    Date joining_date;
+    String degree;
+    String specialization;
+    Date birth_date;
+    int contact_number;
+    String city;
+    String email;
+
+    public int getDoctor_id() {
+        return doctor_id;
+    }
+
+    public void setDoctor_id(int doctor_id) {
+        this.doctor_id = doctor_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getJoining_date() {
+        return joining_date;
+    }
+
+    public void setJoining_date(Date joining_date) {
+        this.joining_date = joining_date;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public int getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(int contact_number) {
+        this.contact_number = contact_number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
