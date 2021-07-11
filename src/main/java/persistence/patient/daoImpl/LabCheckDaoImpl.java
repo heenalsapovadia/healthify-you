a9e54ba4b3ded7c00a94a3c6be3e0dc87ef5beba
heenalsapovadia <heenalsapovadia@gmail.com>
2021-07-11 14:35:50 +0530

    Ticket#CSCI5308-24 Implemented LabCheckDaoImpl and LabCheckBookingDaoImpl

--- src/main/java/persistence/patient/daoImpl/LabCheckDaoImpl.java
+++ src/main/java/persistence/patient/daoImpl/LabCheckDaoImpl.java
@@ -1,17 +1,47 @@
 package persistence.patient.daoImpl;
 
+import persistence.doctor.daoImpl.PrescriptionDAOImpl;
 import persistence.patient.dao.LabCheckDao;
 import persistence.patient.model.LabCheck;
+import presentation.startup.DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LabCheckDaoImpl implements LabCheckDao {
+    private static final Logger LOGGER = Logger.getLogger(LabCheckDaoImpl.class.getName());
 
     /*
     Fetch all the plans from database
      */
     @Override
     public List<LabCheck> getAvailablePlans() {
+        Connection conn = DatabaseConnection.getConnection();
+
+        List<LabCheck> labCheckList = new ArrayList<>();
+        String sql = "SELECT * FROM labcheck_plans";
+        try(PreparedStatement ps = conn.prepareStatement(sql)){
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                LabCheck labCheck = new LabCheck();
+                labCheck.setCheckup_id(rs.getInt("checkup_id"));
+                labCheck.setCheckup_name(rs.getString("checkup_name"));
+                labCheck.setCheckup_type(rs.getString("checkup_type"));
+                labCheck.setDescription(rs.getString("description"));
+                labCheckList.add(labCheck);
+            }
+            return labCheckList;
+        }
+        catch (SQLException e){
+            LOGGER.log(Level.SEVERE, e.toString());
+            System.out.println("SQL ERROR:"+e.getMessage());
+        }
         return null;
     }
 }
