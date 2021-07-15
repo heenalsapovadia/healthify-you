7998e1d29dc43c6e0dc4f2cd080d9b39311e1651
heenalsapovadia <heenalsapovadia@gmail.com>
2021-07-15 21:52:13 +0530

    Ticket#CSCI5308-24 LabCheckBooking - Added Test and implementation for genderBased Recommendation

--- src/main/java/persistence/patient/utilImpl/LabCheckRecommendationUtilImpl.java
+++ src/main/java/persistence/patient/utilImpl/LabCheckRecommendationUtilImpl.java
@@ -1,18 +1,37 @@
 package persistence.patient.utilImpl;
 
+import persistence.patient.dao.LabCheckDao;
+import persistence.patient.daoImpl.LabCheckDaoImpl;
 import persistence.patient.model.LabCheck;
+import persistence.patient.model.Patient;
 import persistence.patient.util.LabCheckRecommendationUtil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class LabCheckRecommendationUtilImpl implements LabCheckRecommendationUtil {
+    HashMap<Integer, LabCheck> labCheckMap;
     @Override
     public List<LabCheck> genderBasedRecommendation() {
-        return null;
+        if(labCheckMap == null)
+            setLabCheckMap();
+        List<LabCheck> recommendations = new ArrayList<>();
+        if(Patient.getPatient().getPatientGender().equals("F"))
+            recommendations.add(labCheckMap.get(4));
+        return recommendations;
     }
 
     @Override
     public List<LabCheck> ageBasedRecommendation() {
         return null;
+    }
+
+    private void setLabCheckMap(){
+        LabCheckDao labCheckDao = new LabCheckDaoImpl();
+        List<LabCheck> labCheckList = labCheckDao.getAvailablePlans();
+        labCheckMap = new HashMap<>();
+        for(LabCheck labCheck : labCheckList)
+            labCheckMap.put(labCheck.getCheckup_id(), labCheck);
     }
 }
--- src/test/java/persistence/patient/utilImpl/LabCheckRecommendationUtilImplTest.java
+++ src/test/java/persistence/patient/utilImpl/LabCheckRecommendationUtilImplTest.java
@@ -1,13 +1,35 @@
 package persistence.patient.utilImpl;
 
 import org.junit.Test;
+import persistence.patient.model.LabCheck;
+import persistence.patient.model.Patient;
+import persistence.patient.util.LabCheckRecommendationUtil;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class LabCheckRecommendationUtilImplTest {
 
     @Test
-    public void genderBasedRecommendation() {
+    public void genderBasedRecommendationForFemale() {
+        Patient.setPatient("female_patient@gmail.com");
+        Patient.getPatient().setPatientGender("F");
+
+        LabCheckRecommendationUtil labCheckRecommendationUtil = new LabCheckRecommendationUtilImpl();
+        List<LabCheck> labCheckList = labCheckRecommendationUtil.genderBasedRecommendation();
+        assertEquals(1, labCheckList.size());
+        assertEquals(4, labCheckList.get(0).getCheckup_id());
+    }
+
+    @Test
+    public void genderBasedRecommendationForNonFemale() {
+        Patient.setPatient("Nonfemale_patient@gmail.com");
+        Patient.getPatient().setPatientGender("M");
+
+        LabCheckRecommendationUtil labCheckRecommendationUtil = new LabCheckRecommendationUtilImpl();
+        List<LabCheck> labCheckList = labCheckRecommendationUtil.genderBasedRecommendation();
+        assertEquals(0, labCheckList.size());
     }
 
     @Test
