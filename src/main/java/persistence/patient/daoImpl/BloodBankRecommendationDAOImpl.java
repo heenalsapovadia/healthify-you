a1e372ed2707b2be2f5570b10df5cce5f2a46612
samikshasalgaonkar <84499600+samikshasalgaonkar@users.noreply.github.com>
2021-07-19 14:33:14 +0530

    Ticket#CSCI5308-15 Added fetchBloodGroupList() method to the BloodBankRecommendationDAOImpl java file

--- src/main/java/persistence/patient/daoImpl/BloodBankRecommendationDAOImpl.java
+++ src/main/java/persistence/patient/daoImpl/BloodBankRecommendationDAOImpl.java
@@ -1,7 +1,12 @@
 package persistence.patient.daoImpl;
 
 import persistence.admin.dao.BloodBankRecommendationDAO;
+import presentation.startup.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class BloodBankRecommendationDAOImpl implements BloodBankRecommendationDAO {
@@ -9,7 +14,31 @@ public class BloodBankRecommendationDAOImpl implements BloodBankRecommendationDA
   @Override
   public ArrayList<String> fetchBloodGroupList(String startDate, String endDate) throws SQLException {
 
-    return null;
+    Connection conn = DatabaseConnection.getConnection();
+    Statement statement = conn.createStatement();
+    ResultSet rS = null;
+
+    String sql = "select sum(quantity) as sum_quantity, blood_group from blood_bank_orders where order_date between \"" + startDate + "\" and \"" + endDate + "\" group by blood_group order by sum_quantity desc, blood_group;";
+
+    try {
+      /* retrieves blood group list for the symptoms */
+      rS = statement.executeQuery(sql);
+
+      ArrayList<String> bloodGroupList = new ArrayList<>();
+      while (rS.next()) {
+        bloodGroupList.add(rS.getString("blood_group"));
+      }
+
+      if(bloodGroupList.isEmpty()) {
+        System.out.println("Nothing to recommend!");
+        return bloodGroupList;
+      } else {
+        return bloodGroupList;
+      }
+
+    } catch (SQLException se) {
+      return null;
+    }
 
   }
 
