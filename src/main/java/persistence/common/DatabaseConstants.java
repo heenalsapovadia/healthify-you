3b9dad22b4cd055f7bb4987824879c13796cfe4a
sapovadia <hn525278@dal.ca>
2021-07-26 12:28:59 +0530

    Ticket#CSCI5308-5 Adding DatabaseConstants for AppointmentDAOImpl

--- src/main/java/persistence/common/DatabaseConstants.java
+++ src/main/java/persistence/common/DatabaseConstants.java
@@ -16,7 +16,9 @@ public class DatabaseConstants {
     /* Common Appointment constants */
     public static final String APPOINTMENT_ID = "appointment_id";
     public static final String DOCTOR_ID = "doctor_id";
+    public static final String BOOKED_ON_DATE = "booked_on_date";
     public static final String BOOKED_FOR_DATE = "booked_for_date";
+    public static final String RESCHEDULED_DATE = "rescheduled_date";
 
     /* Patient Table constants */
     public static final String PATIENT_ID = "patient_id";
@@ -28,4 +30,7 @@ public class DatabaseConstants {
     public static final String VACCINE_NAME = "vaccine_name";
     public static final String VACCINE_ID = "vaccine_id";
     public static final String AGE_GROUP_IN_YEARS = "age_group_in_years";
+
+    /* Billing table constants */
+    public static final String BILLING_ID = "billing_id";
 }
