407b6924b86f6a8737d39d0ec71d22e62c35c228
sapovadia <hn525278@dal.ca>
2021-07-26 13:33:46 +0530

    Ticket#CSCI5308-5 Adding JSONConstants in JsonIdealReportParserImpl

--- src/main/java/persistence/common/JSONConstants.java
+++ src/main/java/persistence/common/JSONConstants.java
@@ -1,7 +1,36 @@
 package persistence.common;
 
+/**
+ * <pre>
+ * Contains all the common constants representing JSON file keys
+ * required throughout the application.
+ * </pre>
+ *
+ * @author G12
+ *
+ */
 public class JSONConstants {
 
     private JSONConstants() {}
+
+    public static final String IDEAL_REPORTS_FILENAME = "IdealReports.json";
+    public static final String RBC_MIN = "RBCMin";
+    public static final String WBC_MIN = "WBCMin";
+    public static final String PLATELETS_MIN = "plateletsMin";
+    public static final String HAEMOGLOBIN_MIN = "HaemoglobinMin";
+    public static final String HEMATOCRIT_MIN = "HematocritMin";
+    public static final String RBC_MAX = "RBCMax";
+    public static final String WBC_MAX = "WBCMax";
+    public static final String PLATELETS_MAX = "plateletsMax";
+    public static final String HAEMOGLOBIN_MAX = "HaemoglobinMax";
+    public static final String HEMATOCRIT_MAX = "HematocritMax";
+    public static final String BUN_MIN = "BUNMin";
+    public static final String BUN_MAX = "BUNMax";
+    public static final String CREATININE_MAX = "CreatinineMax";
+    public static final String ALBUMIN_MIN = "AlbuminMin";
+    public static final String BILIRUBIN_MIN = "BilirubinMin";
+    public static final String ALT_MAX = "ALTMax";
+    public static final String AST_MAX = "ASTMax";
+    public static final String ALP_MAX = "ALPMax";
 
 }
