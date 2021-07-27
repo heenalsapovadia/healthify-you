c4bd4d5868d9339ce126d6a3b51af07b2c548080
Deeksha Sareen <dk930654@dal.ca>
2021-07-27 18:00:17 +0545

    added document comments to the code

--- src/main/java/persistence/startup/util/UserLoginUtil.java
+++ src/main/java/persistence/startup/util/UserLoginUtil.java
@@ -7,8 +7,16 @@ package persistence.startup.util;
 
 public interface UserLoginUtil {
 
+  /**
+   * @param email
+   * @return String
+   */
   public String validateEmail(String email);
 
+  /**
+   * @param password
+   * @return String
+   */
   public String validatePassword(String password);
   
 }
